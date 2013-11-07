928bd874a727c7e2cb8ae3ff1047ba3f2633bd4a
Yudha H Tejaningrat <yudha_ht@yahoo.com>
2013-11-07 09:36:35 +0700

    Sesuaikan Hak Akses Berkas

--- bash/wordpress-update.sh
+++ bash/wordpress-update.sh
@@ -12,10 +12,17 @@ tar xvf wp.tar.gz
 # Salin berkas konfigurasi
 cp wordpress.old/wp-config.php wordpress
 
+# Sesuaikan hak akses berkas konfigurasi
+chown nobody.nogroup wordpress/wp-config.php
+chmod 644 wordpress/wp-config.php
+
 # Salin berkas-berkas pendukung
 rsync -avz wordpress.old/wp-content/uploads/ wordpress/wp-content/uploads/
 rsync -avz wordpress.old/wp-content/themes/ wordpress/wp-content/themes/
 rsync -avz wordpress.old/wp-content/plugins/ wordpress/wp-content/plugins/
+
+# Sesuaikan hak akses berkas-berkas pendukung
+chown -R nobody.nogroup wordpress/wp-content/
 
 # Paketkan backup
 tar cf wordpress-`date '+%Y%m%d'`.tar wordpress.old/
