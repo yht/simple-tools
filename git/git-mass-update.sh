3cd7207022c04ecf3bbf3aad58ade8f83968ed5a
Yudha H Tejaningrat <yudha_ht@yahoo.com>
2016-02-20 07:03:31 +0700

    git: mass update git projects only [fix]

--- git/git-mass-update.sh
+++ git/git-mass-update.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
-for i in *;
- do if [ -d "$i" ];
-  then echo "Sinkronisasi $i"; cd "$i"; git pull; cd ..;
+for i in *
+ do
+ if [ -d "$i/.git" ]
+ then
+  echo "Sinkronisasi $i"
+  cd "$i"
+  git pull
+  cd ..
  fi done
