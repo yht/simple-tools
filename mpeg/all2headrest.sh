5e4fcfb454c01efe9713efb8172d20732f5178e0
Yudha H Tejaningrat <yudha_ht@yahoo.com>
2017-06-19 04:04:55 +0700

    mpeg: v2headrest fix IPS

--- mpeg/all2headrest.sh
+++ mpeg/all2headrest.sh
@@ -1,14 +1,19 @@
 #!/bin/bash
+SAVEIFS=$IFS
+IFS=$(echo -en "\n\b")
 
 # Berkas ini untuk menyimpan pengaturan output video
 # agar bisa diputar di headrest monitor.
 
 # Folder dimana hasil keluaran di simpan
-Folder="/path/to/output/video/"
+Folder="$1"
 
-for i in `ls`
+for i in $(ls)
 do
+ echo "$i"
  avconv -y -i "$i" -f dvd -target pal-dvd -b:v 5000k -r 25 \
     -filter:v scale=640:480 -ar 48000 -b:a 44k -b:a 44k \
     "$Folder$(echo `basename -s .mp4 $i`).mpg"
 done
+
+IFS=$SAVEIFS
